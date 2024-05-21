a6a30abc7cb424e3e793f983ac2470ebba130224
AR-Raju <asikurrahmanraju@gmail.com>
2024-05-21 15:25:44 +0600

    adding business logic for creating order

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,8 +1,39 @@
+import { Product } from "../product/product.model";
 import { IOrder } from "./order.interface";
 import { Order } from "./order.model";
 
 const createOrderIntoDB = async (orderData: IOrder) => {
-  const result = Order.create(orderData);
+  const { productId, quantity } = orderData;
+
+  const product = await Product.findOne({ _id: productId });
+
+  // Check if product is found
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  // Check if there is enough inventory
+  if (product.inventory.quantity < quantity) {
+    // Update the inStock property if needed
+    if (product.inventory.inStock) {
+      product.inventory.inStock = false;
+      await product.save();
+    }
+
+    throw new Error("Insufficient quantity available in inventory");
+  }
+
+  // Reduce the inventory quantity
+  product.inventory.quantity -= quantity;
+
+  // Update the inStock property if quantity goes to zero
+  if (product.inventory.quantity === 0) {
+    product.inventory.inStock = false;
+  }
+
+  await product.save();
+
+  const result = await Order.create(orderData);
   return result;
 };
 
